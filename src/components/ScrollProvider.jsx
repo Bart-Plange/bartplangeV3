96c6a2a1a80741eaa176aad1ecb7504024c988a2
Bart-Plange <bartplangedennis@gmail.com>
2024-05-18 14:10:34 +0000

    Made changes to Smooth scrolling

---
+++ src/components/ScrollProvider.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
+
+const ScrollProvider = ({ children }) => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true,
+      duration: 2,
+      easing: (t) => Math.sin((t * Math.PI)/2),
+      direction: 'vertical',
+      gestureDirection: 'both',
+      smoothTouch: true,
+    });
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default ScrollProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ReactLenis } from '@studio-freight/react-lenis';
+import ScrollProvider from './components/ScrollProvider.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ReactLenis root>
-    <App />
+      <ScrollProvider>
+        <App />
+      </ScrollProvider>  
     </ReactLenis>
   </React.StrictMode>,
 )
